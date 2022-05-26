f2e502bf56a4094383ab6c108c52b64621845515
Matthew <mattea@users.noreply.github.com>
2022-05-26 00:40:03 -0700

    Adding role code.

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
-import { API_KEY } from "./api-key";
+import { API_KEY, SPACE_ID, ADMIN_ID, PASSWORD } from "./api-key";
 import { Game, MoveDirection } from "@gathertown/gather-game-client";
 global.WebSocket = require("isomorphic-ws");
 
-const SPACE_ID = "oxrhEtb3sV7VutbQ\\GatherOffice";
+// Example for api-key file
+/*
+export const API_KEY = "theapikey";
+// SpaceId is what follows app/ with forward slashes as backslashes. E.g.
+// for https://app.gather.town/app/ASDF/my_room
+// this ID is  "ASDF\\my_room"
+export const SPACE_ID = "space_id\\name";
+export const ADMIN_ID = "myplayerid";
+// Password to use for admin control.
+// _NOT SECURE_ Don't use any important password.
+export const PASSWORD = "mypassword";
+*/
 
 // setup
 
@@ -10,14 +21,57 @@ const game = new Game(() => Promise.resolve({ apiKey: API_KEY }));
 game.connect(SPACE_ID); // replace with your spaceId of choice
 game.subscribeToConnection((connected) => console.log("connected?", connected));
 
-//
+var cards = [
+	"Killer", "Killer", "President", "Assassin", "Hunter", "Gatherer"
+];
+
+var admins = new Set();
+var myId;
+admins.add(ADMIN_ID);
+
+function shuffleArray(array: any[]) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+}
+
+function sendRoles(myId: string) {
+	shuffleArray(cards);
+	var pIdx = 0;
+	Object.keys(game.players).forEach((playerId) => {
+		if (playerId == myId) return;
+		game.chat(playerId, [], "", cards[pIdx]);
+		pIdx++;
+	});
+}
+
+function setRoles(message: string) {
+	cards = message.split(':')[1].split(',').map((e) => e.trim());
+}
+
 
 // listen for chats and move
 game.subscribeToEvent("playerChats", (data, _context) => {
+	// console.log("playerChats");
 	// console.log(data);
+	// console.log(_context);
 	const message = data.playerChats;
+	// console.log(message);
 	if (message.messageType === "DM") {
-		// do something
+		if (admins.has(message.senderId)) {
+			if (message.contents.toLowerCase().startsWith("roles:")) {
+				setRoles(message.contents);
+			}
+			switch (message.contents.toLowerCase()) {
+				case "play":
+					sendRoles(message.recipient);
+				default:
+					let reply = "what? try sending play or \"roles: A,B,C,D...\". Or up/down/left/right/dance for fun.";
+					game.chat(message.senderId, [], "", reply);
+			}
+			return;
+		}
 		switch (message.contents.toLowerCase()) {
 			case "up":
 				game.move(MoveDirection.Up);
@@ -33,6 +87,9 @@ game.subscribeToEvent("playerChats", (data, _context) => {
 				break;
 			case "dance":
 				game.move(MoveDirection.Dance);
+				break;
+			case PASSWORD:
+				admins.add(message.senderId);
 				break;
 			default:
 				let reply = "what? try sending up/down/left/right";
@@ -50,7 +107,7 @@ setTimeout(() => {
 	game.engine.sendAction({
 		$case: "setName",
 		setName: {
-			name: "Twitchy",
+			name: "RoboGameMaster",
 		},
 	});
 	game.engine.sendAction({
@@ -59,11 +116,4 @@ setTimeout(() => {
 			textStatus: "DM me to move!",
 		},
 	});
-	// game.engine.sendAction({
-	// 	$case: "setOutfitString",
-	// 	setOutfitString: {
-	// 		outfitString:
-	// 			'{"skin":{"id":"KPK1RNe5O32vJ8IhOicy","type":"skin","isDefault":true,"color":"3","name":"typical","parts":[{"spritesheet Id":"dQCYs4n7O99ksXuBIe33","layerId":"skin front"}],"previewUrl":"https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/wearables/BbNpZNRQylqIUmzc2QveW"},"hair":{"id":"oVHqqja061UDjdFbMaXY","type":"hair","color":"purple","previewUrl":"https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/wearables/_bMY9TZosM6aO3j5uaEDt","parts":[{"layerId":"hair front","spritesheetId":"nxznoKKe5eI2XLkrhU8T"}],"isDefault":true,"name":"messy"},"facial_hair":{},"top":{"id":"1iczssuelMf8BcOMkWjr","parts":[{"layerId":"top front","spritesheetId":"5YkEGqYP4tm6C9J3HBtX"}],"color":"red","name":"shirt (half untucked)","previewUrl":"https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/wearables/cYsgnqhSKY6JbdRIkX52D","isDefault":true,"type":"top"},"bottom":{"id":"Xx1p8Kjv0qEg6WPV6niD","name":"pants","type":"bottom","isDefault":true,"color":"white","previewUrl":"https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/wearables/WeY5PGdf8025J8o2VE8kp","parts":[{"spritesheetId":"TChgCSU6xSHuNmR2PI7H","layerId":"bottom front"}]},"shoes":{"id":"jWRxPyatM2P0bdzSnf50","parts":[{"spritesheetId":"yFpcQh7UcvdChVN8WvIW","layerId":"shoes front"}],"isDefault":true,"previewUrl":"https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/wearables/kgDMGDXcC-ChLVRhOr7TZ","name":"generic","color":"black","type":"shoes"},"hat":{},"glasses":{"id":"KdFp1XwJet15FjqdQa38","isDefault":false,"parts":[{"layerId":"glasses front","spritesheetId":"py34w1ylAbJPYocLYKkq"}],"previewUrl":"https://cdn.gather.town/storage.googleapis.com/gather-town.appspot.com/wearables/_hDx5L7uo8UOmsyFeSrZz","type":"glasses","color":"black","name":"sunglasses"},"other":{},"costume":{}}',
-	// 	},
-	// });
 }, 2000); // wait two seconds before setting these just to give the game a chance to init
